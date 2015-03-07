0de7a99a0a2efef87ebf212cabba074f7e1cf3de
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-03-07 01:48:31 -0500

    Completed 'Animation', completed 'AJAX'
    Apparently I haven’t made a commit to this branch upon completing
    ‘Animation’, but it’s been completed a long time ago and I’ve actually
    just completed ‘AJAX’.
    
    This was “Project Part 3: AJAX”.
    
    - Register.html, Login.html, and most other .html documents -
    Remove ‘This is the main index of the application’ comments
    Change input `id` names
    
    - Projects.html, Dashboard.html -
    Move links to jQuery and `Main.js` to the `<head>` of some documents
    (for data element iterations)
    Reset title of modal view tooltip asking the user for their name as
    opposed to the name of the project
    Remove static code with dynamically loaded data
    
    - Projects.css -
    Add styles for buttons
    
    - Main.css -
    Add style for notification that no data could be found
    
    - Dashboard.css -
    Margin and width changes
    
    - TableLinksies.js -
    Attempt to provide a more semantic description
    
    - pwa2_db.sql -
    Remove (It shouldn’t have been here in the first place. Sorry.)
    
    - Main.js -
    Add event methods (namely `.click()`)
    Add AJAX for validation, account creation, etcetera.
    
    - FetchProjects.js, FetchTasks.js, and FetchUsers.js -
    Create and call from their respective pages for dynamically loading
    content into the table views and tabbed views

---
+++ Wk3/Crammer_Alexander_Project/Assets/Scripts/FetchTasks.js
@@ -0,0 +1,57 @@
+/*  AJAX
+    FetchTasks.js
+    Friday, 6 March, 2015
+    Alexander Rhett Crammer  */
+
+/* Document Overview: Fill a list with task data retrieved dynamically */
+
+$(document).ready(function () {
+    $.ajax({
+        "url":"Assets/Scripts/xhr/get_tasks.php",
+        "type":"GET",
+        "dataType":"JSON",
+        "success": function (fetchTasksResponse) {
+            if (fetchTasksResponse.error) {
+                /* There was a problem fetching the tasks */
+                alert("Your tasks couldn't be loaded.");
+                console.log(fetchTasksResponse.error);
+            } else {
+                /* There wasn't an error */
+                for (var taskIndex=0;taskIndex < fetchTasksResponse.tasks.length;taskIndex++) {
+                    var taskAtCurrentIndex = fetchTasksResponse.tasks[taskIndex];
+                    var tastElement = '<li id="' + taskAtCurrentIndex["id"] + '">';
+                    tastElement += "<p>" + taskAtCurrentIndex["taskName"] + "</p>";
+                    if (window.location.pathname == "Dashboard.html") {
+                        /* The user is visiting the 'Dashboard' page */
+                        tastElement += "<p>" + taskAtCurrentIndex["taskDescription"] + "</p></li>";
+                    }
+                    $(".tasks").append(tastElement);
+                    $(".delete-button").click(function () {
+                        $.ajax({
+                            "url":"Assets/Scripts/xhr/delete_task.php",
+                            "data": {
+                                "taskID": taskAtCurrentIndex.id
+                            },
+                            "type":"POST",
+                            "dataType":"JSON",
+                            "success": function (deleteTaskResponse) {
+                                if (deleteTaskResponse.error) {
+                                    alert(deleteTaskResponse.error);
+                                } else {
+                                    window.location.reload();
+                                }
+                            }
+                        });
+                    });
+                }
+                if (fetchTasksResponse.tasks.length == 0) {
+                    /* There are no tasks to display so we'll note the user of that */
+                    if (!window.location.pathname == "Dashboard.html") {
+                        var noTasksElement = '<p class="noListElements">You don\'t have any tasks at the moment.</p>';
+                        $(".tasks").replaceWith(noTasksElement);
+                    }
+                }
+            }
+        }
+    });
+});
