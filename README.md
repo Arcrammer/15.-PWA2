0daebe0844df004a0c48d595e06c8038852f050a
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-03-07 23:18:02 -0500

    Reorganisation
    I’ll be uploading assignment assets related to each week separately. If
    you’re interested in seeing all of my work completed so far consider
    browsing this branch at its’ state before this commit.
    
    Thanks! :D

--- README.md
+++
@@ -1,3 +0,0 @@
-# Programming for Web Applications II
-
-This course exposes students to jQuery as well as AJAX and AHAH.
--- Wk2/Crammer_Alexander_PWA2/Assets/Scripts/Main.js
+++
@@ -1,8 +0,0 @@
-/*  Application Setup
-    Main.js
-    Tuesday, 3 March, 2015
-    Alexander Rhett Crammer  */
-
-$(document).ready(function () {
-    /* The DOM has loaded and we're ready to execute scripts */
-});
